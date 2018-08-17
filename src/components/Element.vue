05725eb24253e78535b5c9d0f00a56f1a70d8b46
Robert Thomas <robert.jeffrey.thomas@gmail.com>
2018-08-17 16:56:34 +0000

    added element-ui

---
+++ src/components/Element.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="element">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Element'
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Element.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="element">
+    <h3>button</h3>
+    <el-button>Default</el-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Element',
+  components: 'el-button'
+}
+
+</script>
+
+<style>
+
+</style>
